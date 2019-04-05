2fcb0a505cd201e05561a8556c0894db1c683c34
Dana Dittoe <dkdittoe@uark.edu>
2019-04-05 11:04:40 -0500

    Added Parse Argument

--- parseGFF.py
+++ parseGFF.py
@@ -1,13 +1,22 @@
 #! /usr/bin/env python
 
-#watermelon.fsa
-#watermelon.gff
+#add parse argument
 
-my_data = open("watermelon.gff", "r")
-data2 = open("watermelon.fsa", "r")
+import argparse
 
-#the variable 'genome' holds the genome sequence
-genome = " "
+#create and argumentparser object ('parser') that will hold all of the information necessary to parse the command line
+parser = argparse.ArgumentParser(description='generates genetic code from genome for each feature')
+
+#add optional arguments
+# -- infront of argument identifies it as optional
+parser.add_argument('-d', '--my_data', help='the .gff  file', default= 'watermelon.gff')
+parser.add_argument('-d2', '--data2', help='the .fsa file', default = 'watermelon.fsa')
+
+#parse the command line arguments
+args = parser.parse_args()
+
+my_data = open(args.my_data, "r")
+data2 = open(args.data2, "r")
 
 #read gff line by line
 for line in my_data:
@@ -15,17 +24,19 @@ for line in my_data:
 
     #remove line breaks
     line = line.rstrip("\n")
-    #print(num)
+    
     #split each line on the tab character
     # sequence, source, feature, begin, end, length, strand, phase, attributes = line.split("\t")
     fields = line.split("\t")
     print(fields[3], fields[4])
     start = int(fields[3])
     stop = int(fields[4])
+    
     #extract the dna sequence from the genome for this feature
     substring = data2[start:stop]
     genome = genome + substring
     print(genome)
+    
     #print GC content
     G = str(genome).count('G')
     C = str(genome).count('C')

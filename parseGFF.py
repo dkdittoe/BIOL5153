b596d0dd03583f09e5dfb4eaad6ed3449000c2ba
Dana Dittoe <dkdittoe@uark.edu>
2019-04-14 22:35:27 -0500

    Added SeqIO and CSV Reader

--- parseGFF.py
+++ parseGFF.py
@@ -1,49 +1,55 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
-#add parse argument
+#------------------------------------------------
 
-import argparse
+#the variable 'genome' holds the genome sequence
+genome = []
 
-#create and argumentparser object ('parser') that will hold all of the information necessary to parse the command line
-parser = argparse.ArgumentParser(description='generates genetic code from genome for each feature')
+#------------------------------------------------
 
-#add optional arguments
-# -- infront of argument identifies it as optional
-parser.add_argument('-d', '--my_data', help='the .gff  file', default= 'watermelon.gff')
-parser.add_argument('-d2', '--data2', help='the .fsa file', default = 'watermelon.fsa')
+# bio python to parse fsa file
+from Bio import SeqIO
 
-#parse the command line arguments
-args = parser.parse_args()
+fasta_sequences = SeqIO.parse(open('watermelon.fsa'),'fasta')
+for fasta in fasta_sequences:
+    name, sequence = fasta.id, str(fasta.seq)
 
-my_data = open(args.my_data, "r")
-data2 = open(args.data2, "r")
+#------------------------------------------------
 
-#read gff line by line
-for line in my_data:
-    #skip blank lines
+#csv to parse gff file
+import csv
 
-    #remove line breaks
-    line = line.rstrip("\n")
-    
-    #split each line on the tab character
-    # sequence, source, feature, begin, end, length, strand, phase, attributes = line.split("\t")
-    fields = line.split("\t")
-    print(fields[3], fields[4])
-    start = int(fields[3])
-    stop = int(fields[4])
-    
-    #extract the dna sequence from the genome for this feature
-    substring = data2[start:stop]
-    genome = genome + substring
-    print(genome)
-    
-    #print GC content
-    G = str(genome).count('G')
-    C = str(genome).count('C')
-    total = len(str(genome))
-    GC_total = G+C
-    GC_content = GC_total/total
-    print(GC_content)
+#open data file
+with open('watermelon.gff', 'r') as gff_file:
+
+	#create a csv reader object
+	#have to give the reader what the delimiter is, i.e. comma 
+	csvreader = csv.reader(gff_file, delimiter='\t')
+	
+	#split into a list and can now access column, by column...parsed the file
+	for line in csvreader:
+	#conditional formatting to skip empty lines
+		if not line:
+			continue
+		else:		
+			start = int([3])
+    		stop = int([4])
+
+
+#---------------------------------------------------
+
+            #extract the dna sequence from the genome for this feature
+            substring = sequence[start:stop]
+            genome = genome + substring
+            print(genome)
+
+            #print GC content
+            G = str(genome).count('G')
+            C = str(genome).count('C')
+            total = len(str(genome))
+            GC_total = G+C
+            GC_content = GC_total/total
+            print(GC_content)
 
 #write the code for extracting the substring
 #caculate the GC content for this feature, and print it to the screen
